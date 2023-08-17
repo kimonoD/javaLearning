79192e6958010d32d697af2c36fa5d650ca60509
Dominik <domino.halamicek@gmail.com>
2023-08-17 18:10:01 +0200

    First commit - first java program

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Sum of 4,5 is " + sum(4, 5));
+        System.out.println("Sum of 5,6 is " + sum(5, 6));
+        System.out.println("Sum of 2,3 is " + sum(2, 3));
+
+        int a = 10;
+        int b = 20;
+        int sum = sum(a, b);
+
+        int p = 2;
+        int d = 3;
+        sum = sum (p, d);
+
+        System.out.println("Sum of from function " + sum);
+
+        //git:
+        //add
+        //commit
+
+
+    //primitive types:
+    //int float double char long
+    private static int sum(int a, int b) {
+        int ffff;
+        return a + b;
+    }
+}
---
+++ src/test/java/test.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class test {
+    @Test
+    public void test()
+    {
+        System.out.println("Hello world.");
+    }
+
+}
