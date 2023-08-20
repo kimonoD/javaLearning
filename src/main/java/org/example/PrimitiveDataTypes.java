87e32c1cb2373094229a5c40cb05fc69c68ebbf7
Dominik <domino.halamicek@gmail.com>
2023-08-20 13:03:24 +0200

    Playing with primitive data types.

---
+++ src/main/java/org/example/PrimitiveDataTypes.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class PrimitiveDataTypes {
+
+    public static void playWithBooleans() {
+        boolean hasCar;
+        boolean hasGarage;
+        hasCar = true;
+        hasGarage = false;
+        System.out.println("I have car " + hasCar);
+        System.out.println("I have garage " + hasGarage);
+    }
+
+    public static boolean iHaveFunctionalChair(boolean hasLegs, boolean hasSeat){
+        return hasLegs && hasSeat;
+    }
+
+
+
+
+
+
+}
