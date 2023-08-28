505165d039c3ffc954de05ab872a8e3673cbb888
Dominik <domino.halamicek@gmail.com>
2023-08-28 12:33:59 +0200

    Homework assignment.

--- src/test/java/TestingArray.java
+++ src/test/java/TestingArray.java
@@ -19,5 +19,10 @@ public class TestingArray {
     public void test3() {
         OneDimensionalArray.iteratingOverArray();
     }
-
+    @Test
+    public void testHomework() {
+        //compute sum of 10 integers, print them all and print sum
+        //after that, change 4. element to -10; and compute sum again
+        //print 4. element
+    }
 }
