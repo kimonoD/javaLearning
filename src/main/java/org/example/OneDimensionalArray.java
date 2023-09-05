f8344fc5efedf2db4f8878820e558b1e6729cdfd
Dominik <domino.halamicek@gmail.com>
2023-09-05 20:37:55 +0200

    Learning for loop.

--- src/main/java/org/example/OneDimensionalArray.java
+++ src/main/java/org/example/OneDimensionalArray.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class OneDimensionalArray {
@@ -35,7 +34,7 @@ public class OneDimensionalArray {
         System.out.println("Array has length: " + array.length);
         System.out.println("Array has length: " + Arrays.toString(array));
 
-        array  = new String[5];
+        array = new String[5];
         System.out.println("Array has length: " + Arrays.toString(array));
     }
 
@@ -56,4 +55,72 @@ public class OneDimensionalArray {
 
     }
 
+    public static void sumViaStream() {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = Arrays.stream(array).sum();
+
+        System.out.println("Sum of all the elements of an array: " + sum);
+        System.out.println("Array has length: " + array.length);
+        System.out.println("Array numbers are: " + Arrays.toString(array));
+    }
+
+    public static int sumViaRawLoop() {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        //motivation:
+        int sum = array[0] + array[1];
+        sum = array[0] + array[1] + array[2];
+        //sum = array[0] + array[1] + array[2] + ... + array[9];
+
+        //motivation
+       /* sum = 0;
+        sum = sum + array[0];
+        sum = sum + array[1];
+        sum = sum + array[2];
+        //...
+        sum = sum + array[9];
+       */
+
+        sum = 0;
+        //index = index + 1; <=> index ++ <=> index+=1;
+        for (int index = 0; index < array.length; index++) {
+            sum = sum + array[index];
+        }
+
+        sum = array[0];
+        for (int index = 1; index < array.length; index++) {
+            sum = sum + array[index];
+        }
+
+        sum = array[0];
+        for (int index = 1; index <= array.length - 1; index++) {
+            sum = sum + array[index];
+        }
+
+        sum = array[0];
+        for (int index = 1; index <= array.length - 1; index++) {
+            sum += array[index];
+        }
+
+        System.out.println("Sum of all the elements of an array: " + sum);
+        System.out.println("Array has length: " + array.length);
+        System.out.println("Array numbers are: " + Arrays.toString(array));
+
+        return sum;
+    }
+
+    public static int sumViaSimpleForLoop() {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        //motivation:
+        int sum = 0;
+
+        for (int element : array) {
+            sum += element;
+        }
+
+        return sum;
+    }
+
 }
+
+
+
--- src/test/java/TestingArray.java
+++ src/test/java/TestingArray.java
@@ -1,4 +1,5 @@
 import org.example.OneDimensionalArray;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestingArray {
@@ -19,10 +20,25 @@ public class TestingArray {
     public void test3() {
         OneDimensionalArray.iteratingOverArray();
     }
+
     @Test
     public void testHomework() {
+
+        OneDimensionalArray.sumViaStream();
         //compute sum of 10 integers, print them all and print sum
         //after that, change 4. element to -10; and compute sum again
         //print 4. element
+    }
+
+    @Test
+    public void testSum() {
+
+        Assertions.assertEquals(55, OneDimensionalArray.sumViaRawLoop());
+    }
+
+    @Test
+    public void testSum2() {
+
+        Assertions.assertEquals(55, OneDimensionalArray.sumViaSimpleForLoop());
     }
 }
