928789a126ee673511e813dfa71280ba8600ed26
Dominik <domino.halamicek@gmail.com>
2023-09-19 19:35:36 +0200

    NICE TO HAVE: bitwise operators

---
+++ src/main/java/org/example/BitwiseOperators.java
@@ -0,0 +1,5 @@
+package org.example;
+
+// TODO: 19. 09. 2023 nice to have: basic bitwise operators: &,|,~ ,<<,>>  see, pdfDocuments/BitwiseCmd_BitWiseOperators.pdf
+public class BitwiseOperators {
+}
